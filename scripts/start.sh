703c950c7465541710afc8a4ff4d06ab1db07ca4
Faris Aziz <farisaziz12@gmail.com>
2022-02-06 21:29:05 +0100

    chore(scripts): add local start option

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,11 +7,21 @@ SERVER_BASE_URL="http://$IP_ADDRESS:4000"
 
 # server
 if [ "$1" == '--server' ]; then
-    cd ./src/server && IP_ADDRESS="$IP_ADDRESS" yarn dev || exit
+    if [ "$(node -v)" != "v14.0.0" ]; then
+        echo "Failed to start up. You need to be running node v14.0.0"
+        exit 1
+    fi
+
+    if [ "$2" == '--local' ]; then
+        echo "Starting server in local mode"
+        cd ./src/server && yarn dev || exit
+    else
+        echo "Starting server in broadcast mode"
+        cd ./src/server && IP_ADDRESS="$IP_ADDRESS" yarn dev || exit
+    fi
 fi
 
 # lightning-app
-
 if [ "$1" == '--client-serve' ]; then
 
     if ! command -v lng &> /dev/null
